12ab729716523160c424b8e1e7bf6dc9f6ee1885
Kgothatso <Kgothatsochauke3@gmail.com>
2026-02-16 13:14:44 +0200

    Initial comit

---
+++ quizapp-backend/src/test/java/com/kgothatso/quizapp_backend/QuizappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgothatso.quizapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
