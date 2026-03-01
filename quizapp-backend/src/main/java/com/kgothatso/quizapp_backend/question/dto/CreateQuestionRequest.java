4130296e94a0266eb2c7e68f3b173f5bc607255a
Kgothatso <Kgothatsochauke3@gmail.com>
2026-03-01 15:45:18 +0200

    lombok activation

--- quizapp-backend/src/main/java/com/kgothatso/quizapp_backend/question/dto/CreateQuestionRequest.java
+++ quizapp-backend/src/main/java/com/kgothatso/quizapp_backend/question/dto/CreateQuestionRequest.java
@@ -3,8 +3,14 @@ package com.kgothatso.quizapp_backend.question.dto;
 import com.kgothatso.quizapp_backend.question.model.Difficulty;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Component
 public class CreateQuestionRequest {
     private String questionText;
