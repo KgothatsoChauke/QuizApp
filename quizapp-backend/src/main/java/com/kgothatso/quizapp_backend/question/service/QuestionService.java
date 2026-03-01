475862780ca321e98d5fdb1022cb73df4c3b6d06
Kgothatso <Kgothatsochauke3@gmail.com>
2026-03-01 15:40:24 +0200

    feat: retrieve all questions

--- quizapp-backend/src/main/java/com/kgothatso/quizapp_backend/question/service/QuestionService.java
+++ quizapp-backend/src/main/java/com/kgothatso/quizapp_backend/question/service/QuestionService.java
@@ -16,18 +16,23 @@ public class QuestionService {
     private final QuestionRepository questionRepository;
     private final QuestionMapper questionMapper;
 
-    public QuestionService(QuestionRepository questionRepository, QuestionMapper questionMapper) {
+    public QuestionService(QuestionRepository questionRepository, QuestionMapper questionMapper){
         this.questionRepository = questionRepository;
         this.questionMapper = questionMapper;
     }
 
 
     //build a question
-    public QuestionDTO createQuestion(CreateQuestionRequest request) {
+    public QuestionDTO createQuestion(CreateQuestionRequest request){
         Question question = questionMapper.toEntity(request);
         Question saved = questionRepository.save(question);
 
         return questionMapper.toDto(saved);
+    }
+
+    //return all questions
+    public List<QuestionDTO> getAllQuestions(){
+        return questionMapper.toDtoList(questionRepository.findAll());
     }
 
 }
