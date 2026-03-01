3ab897a0b8275ecfa2e2de5d3b64ce39b00cfa25
Kgothatso <Kgothatsochauke3@gmail.com>
2026-03-01 15:37:04 +0200

    feat: create question

--- quizapp-backend/src/main/java/com/kgothatso/quizapp_backend/question/service/QuestionService.java
+++ quizapp-backend/src/main/java/com/kgothatso/quizapp_backend/question/service/QuestionService.java
@@ -1,4 +1,33 @@
 package com.kgothatso.quizapp_backend.question.service;
 
+import com.kgothatso.quizapp_backend.question.dto.CreateQuestionRequest;
+import com.kgothatso.quizapp_backend.question.dto.QuestionDTO;
+import com.kgothatso.quizapp_backend.question.exception.QuestionNotFoundException;
+import com.kgothatso.quizapp_backend.question.mapper.QuestionMapper;
+import com.kgothatso.quizapp_backend.question.model.Question;
+import com.kgothatso.quizapp_backend.question.repository.QuestionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+    private final QuestionMapper questionMapper;
+
+    public QuestionService(QuestionRepository questionRepository, QuestionMapper questionMapper) {
+        this.questionRepository = questionRepository;
+        this.questionMapper = questionMapper;
+    }
+
+
+    //build a question
+    public QuestionDTO createQuestion(CreateQuestionRequest request) {
+        Question question = questionMapper.toEntity(request);
+        Question saved = questionRepository.save(question);
+
+        return questionMapper.toDto(saved);
+    }
+
 }
