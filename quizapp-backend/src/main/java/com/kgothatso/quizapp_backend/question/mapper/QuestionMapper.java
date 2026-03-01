cc2dee3f509326129cc652856e485a218134af91
Kgothatso <Kgothatsochauke3@gmail.com>
2026-03-01 15:47:16 +0200

    add question list to Dto mapper

--- quizapp-backend/src/main/java/com/kgothatso/quizapp_backend/question/mapper/QuestionMapper.java
+++ quizapp-backend/src/main/java/com/kgothatso/quizapp_backend/question/mapper/QuestionMapper.java
@@ -6,9 +6,12 @@ import com.kgothatso.quizapp_backend.question.dto.QuestionDTO;
 import com.kgothatso.quizapp_backend.question.model.Question;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface QuestionMapper {
 
-    QuestionDTO toDTO(Question question);
+    QuestionDTO toDto(Question question);
     Question toEntity(CreateQuestionRequest createQuestionRequest);
+    List<QuestionDTO> toDtoList(List<Question> questions);
 }
